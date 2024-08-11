4f5c26602613311cf6bd8a9f34add1df5972feb5
Dominik <dominik.merdzik@onet.pl>
2024-08-11 12:15:30 +0200

    fixed table

--- src/components/PostmortemTable/PostmortemTable.jsx
+++ src/components/PostmortemTable/PostmortemTable.jsx
@@ -1,46 +1,44 @@
-import React, { useEffect, useState } from "react"
-import { Table } from "react-bootstrap"
-import dumbData from "../../dumb_data.json"
+import React, { useState } from "react";
+import { Table } from "react-bootstrap";
+import dumbData from "../../dumb_data.json";
 
 const PostmortemTable = () => {
-    const [data, setData] = useState(dumbData.postmortems)
+    const [data] = useState(dumbData.postmortems);
 
     return (
-        <Table striped bordered hover>
-            <thread>
+        <Table striped>
+            <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Incident</th>
-                    <th>Prep</th>
-                    <th>Assigned To</th>
-                    <th>Issue Date</th>
-                    <th>In Scope</th>
-                    <th>Comments</th>
-                    <th>Identified Issue</th>
-                    <th>RCA</th>
-                    <th>Technology</th>
+                    <th style={{ width: "5%" }}>ID</th>
+                    <th style={{ width: "15%" }}>Incident</th>
+                    <th style={{ width: "10%" }}>Prep</th>
+                    <th style={{ width: "15%" }}>Assigned To</th>
+                    <th style={{ width: "10%" }}>Issue Date</th>
+                    <th style={{ width: "10%" }}>In Scope</th>
+                    <th style={{ width: "15%" }}>Comments</th>
+                    <th style={{ width: "15%" }}>Identified Issue</th>
+                    <th style={{ width: "10%" }}>RCA</th>
+                    <th style={{ width: "15%" }}>Technology</th>
                 </tr>
-            </thread>
+            </thead>
             <tbody>
-                { data.map(row => {
-                    return (
-                        <tr key={ row.id }>
-                            <td>{ row.id }</td>
-                            <td>{ row.incident }</td>
-                            <td>{ row.prep }</td>
-                            <td>{ row.assigned_to }</td>
-                            <td>{ row.issue_date }</td>
-                            <td>{ row.in_scope }</td>
-                            <td>{ row.comments }</td>
-                            <td>{ row.identified_issue }</td>
-                            <td>{ row.rca }</td>
-                            <td>{ row.technology }</td>
-                        </tr>
-                    )
-                }) }
+                {data.map((row) => (
+                    <tr key={row.id}>
+                        <td>{row.id}</td>
+                        <td>{row.incident}</td>
+                        <td>{row.prep}</td>
+                        <td>{row.assigned_to}</td>
+                        <td>{row.issue_date}</td>
+                        <td>{row.in_scope}</td>
+                        <td>{row.comments}</td>
+                        <td>{row.identified_issue}</td>
+                        <td>{row.rca}</td>
+                        <td>{row.technology}</td>
+                    </tr>
+                ))}
             </tbody>
         </Table>
-    )
-}
+    );
+};
 
-export default PostmortemTable
+export default PostmortemTable;
